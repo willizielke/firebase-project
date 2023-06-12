988aacb1282b4c09fdc5f0e46a2428a2c2640314
Willi Zielke <willi.zielke@allegion.com>
2023-06-12 20:40:52 +0200

    firebase-project setup finished

--- firebase-project/src/app/components/firestorage/firestorage.component.ts
+++ firebase-project/src/app/components/firestorage/firestorage.component.ts
@@ -25,22 +25,15 @@ export class FirestorageComponent {
   }
 
   upload() {}
-  /*   upload(): void {
+  /*upload(): void {
     if (this.selectedFile) {
       const filePath = `uploads/${Date.now()}_${this.selectedFile.name}`;
-      const fileRef = this.storage.ref(filePath);
       const task = this.storage.upload(filePath, this.selectedFile);
-
-      // Optional: Track the upload progress
-      task.percentageChanges().subscribe((percentage) => {
-        console.log(`Upload progress: ${percentage}%`);
-      });
 
       // Get the download URL after the upload is complete
       task.then((snapshot) => {
         snapshot.ref.getDownloadURL().then((url) => {
           this.imageSrc = url;
-          console.log('File available at: ', this.imageSrc);
         });
       });
     }
